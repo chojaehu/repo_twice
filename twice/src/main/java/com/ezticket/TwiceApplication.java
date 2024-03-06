ad5703101f76161e044dbab0cab39a3350b3af75
chojaehu <fhfh5741@gmail.com>
2024-03-06 15:44:42 +0900

    first

---
+++ twice/src/main/java/com/ezticket/TwiceApplication.java
@@ -0,0 +1,13 @@
+package com.ezticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwiceApplication.class, args);
+	}
+
+}
---
+++ twice/src/test/java/com/ezticket/TwiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
