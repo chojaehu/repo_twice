e8929d9d6b2b84b9bdb6e7dccc2d522ba6ce02ff
chojaehu <fhfh5741@gmail.com>
2024-03-12 17:41:42 +0900

    2024-03-12 upload

---
+++ twice/src/main/java/com/ezticket/common/constants/Constants.java
@@ -0,0 +1,17 @@
+package com.ezticket.common.constants;
+
+public class Constants {
+	public static final String DATETIME_FORMAT_BASIC = "yyyy-MM-dd HH:mm:ss";
+	public static final String TIME_FORMAT_BASIC = "HH:mm:ss";
+	
+	public static final int DATE_INTERVAL = -30;;
+	
+	
+
+	
+
+	
+	
+
+
+}
--- twice/src/main/java/com/ezticket/infra/codegroup/CodeGroupController.java
+++ twice/src/main/java/com/ezticket/infra/codegroup/CodeGroupController.java
@@ -3,7 +3,11 @@ package com.ezticket.infra.codegroup;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ezticket.common.util.UtilDateTime;
+import com.ezticket.common.constants.Constants;
 
 @Controller
 public class CodeGroupController {
@@ -131,13 +135,18 @@ public class CodeGroupController {
 	
 	
 	@RequestMapping(value = "/codeGroupXdmList")
-	public String codeGroupXdmList(Model model,CodeGroupVo vo) throws Exception {
+	public String codeGroupXdmList(@ModelAttribute("vo") CodeGroupVo vo,Model model,CodeGroupDto dto) throws Exception {
+		
+		System.out.println("시작일"+vo.getShDateStart());
+		System.out.println("종료일"+vo.getShDateEnd());
+		
+		setSearch(vo);
 		
 		model.addAttribute("list", service.selectList(vo));
 		
-		model.addAttribute("vo", vo);
-		
-		System.out.println(vo.getShDelNy());
+//		model.addAttribute("vo", vo);
+		System.out.println("시작일"+vo.getShDateStart());
+		System.out.println("종료일"+vo.getShDateEnd());
 		
 		return "/xdm/infra/index/codeGroupXdmList";
 	}
@@ -179,6 +188,29 @@ public class CodeGroupController {
 		service.update(dto);
 
 		return "redirect:/codeGroupXdmList";
+	}
+	
+	public void setSearch(CodeGroupVo vo) throws Exception {
+		/* 최초 화면 로딩시에 세팅은 문제가 없지만 */
+		/*이후 전체적으로 데이터를 조회를 하려면 null 값이 넘어 오는 관계로 문제가 전체 데이터 조회가 되지 못한다.*/
+		/*해서 BaseVo.java 에서 기본값을 주어서 처리*/
+//		vo.setShUseNy(vo.getShUseNy() == null ? 1 : vo.getShUseNy());
+//		vo.setShDelNy(vo.getShDelNy() == null ? 0 : vo.getShDelNy());
+//		vo.setShOptionDate(vo.getShOptionDate() == null ? 2 : vo.getShOptionDate());
+		
+		/* 초기값 세팅이 있는 경우 사용 */
+		vo.setShDateStart(vo.getShDateStart() == null
+		    ? UtilDateTime.calculateDayReplace00TimeString(UtilDateTime.nowLocalDateTime(), Constants.DATE_INTERVAL)
+		    : UtilDateTime.add00TimeString(vo.getShDateStart()));
+		vo.setShDateEnd(vo.getShDateEnd() == null
+		    ? UtilDateTime.nowString()
+		    : UtilDateTime.add59TimeString(vo.getShDateEnd()));		
+		
+//		/* 초기값 세팅이 없는 경우 사용 */
+//		vo.setShDateStart(vo.getShDateStart() == null || vo.getShDateStart() == "" ? null : UtilDateTime.add00TimeString(vo.getShDateStart()));
+//		vo.setShDateEnd(vo.getShDateEnd() == null || vo.getShDateEnd() == "" ? null : UtilDateTime.add59TimeString(vo.getShDateEnd()));
+		
+		
 	}
 
 	
