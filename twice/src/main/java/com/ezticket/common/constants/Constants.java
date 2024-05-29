b2c5be34811d8075c719d18b469f7c6244e3fd44
chojaehu <fhfh5741@gmail.com>
2024-05-29 16:47:12 +0900

    엑셀 다운로드 추가

--- twice/src/main/java/com/ezticket/common/constants/Constants.java
+++ twice/src/main/java/com/ezticket/common/constants/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 	public static final String DATETIME_FORMAT_BASIC = "yyyy-MM-dd HH:mm:ss";
 	public static final String TIME_FORMAT_BASIC = "HH:mm:ss";
 	
-	public static final int DATE_INTERVAL = -30;;
+	public static final int DATE_INTERVAL = -60;;
 	
 	public static final int SESSION_MINUTE_XDM = 30;
 	
--- twice/src/main/java/com/ezticket/infra/code/CodeController.java
+++ twice/src/main/java/com/ezticket/infra/code/CodeController.java
@@ -2,8 +2,16 @@ package com.ezticket.infra.code;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,9 +20,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.ezticket.common.constants.Constants;
 import com.ezticket.common.util.UtilDateTime;
 import com.ezticket.infra.codegroup.CodeGroupService;
-import com.ezticket.infra.codegroup.CodeGroupVo;
 
-import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class CodeController {
@@ -129,6 +136,94 @@ public class CodeController {
 		model.addAttribute("listPaging", codeGroupService.selectListWithoutPaging());
 		return cd+"codeXdmInst";
 	}
+	
+	
+	
+	@RequestMapping("/excelDownload")
+    public void excelDownload(@ModelAttribute("vo")CodeVo vo, HttpServletResponse httpServletResponse) throws Exception {
+		
+		setSearch(vo);
+		vo.setParamsPaging(service.count(vo));
+
+		if (vo.getTotalRows() > 0) {
+			List<CodeDto> list = service.selectList(vo);
+			
+			Workbook workbook = new HSSFWorkbook();	// for xls
+//	        Workbook workbook = new XSSFWorkbook();
+	        Sheet sheet = workbook.createSheet("첫번째 시트");
+	        CellStyle cellStyle = workbook.createCellStyle();        
+	        Row row = null;
+	        Cell cell = null;
+	        int rowNum = 0;
+			
+//	        each column width setting
+	        sheet.setColumnWidth(0, 2100);
+	        sheet.setColumnWidth(1, 3100);
+
+//	        Header
+	        String[] tableHeader = {"그룹넘버",	"그룹이름",	"코드넘버","코드이름","등록일","수정일","삭제여부"};
+
+	        row = sheet.createRow(rowNum++);
+			for(int i=0; i<tableHeader.length; i++) {
+				cell = row.createCell(i);
+	        	cellStyle.setAlignment(HorizontalAlignment.CENTER);
+	        	cell.setCellStyle(cellStyle);
+				cell.setCellValue(tableHeader[i]);
+			}
+
+//	        Body
+	        for (int i=0; i<list.size(); i++) {
+	            row = sheet.createRow(rowNum++);
+	            
+//	            String type: null 전달 되어도 ok
+//	            int, date type: null 시 오류 발생 하므로 null check
+//	            String type 이지만 정수형 데이터가 전체인 seq 의 경우 캐스팅
+	            
+	            cell = row.createCell(0);
+	        	cellStyle.setAlignment(HorizontalAlignment.CENTER);
+	        	cell.setCellStyle(cellStyle);
+	            cell.setCellValue(Integer.parseInt(list.get(i).getIfcgSeq()));
+	            
+	            cell = row.createCell(1);
+	        	cellStyle.setAlignment(HorizontalAlignment.CENTER);
+	        	cell.setCellStyle(cellStyle);
+	        	cell.setCellValue(list.get(i).getIfcgName());
+	        	
+	            cell = row.createCell(2);
+	        	cellStyle.setAlignment(HorizontalAlignment.CENTER);
+	        	cell.setCellStyle(cellStyle);
+	        	cell.setCellValue(Integer.parseInt(list.get(i).getIfcdSeq()));
+	        	
+	            cell = row.createCell(3);
+	        	cellStyle.setAlignment(HorizontalAlignment.CENTER);
+	        	cell.setCellStyle(cellStyle);
+	        	cell.setCellValue(list.get(i).getIfcdName());
+	            
+
+	            cell = row.createCell(4);
+	        	cellStyle.setAlignment(HorizontalAlignment.CENTER);
+	        	cell.setCellStyle(cellStyle);
+	        	if(list.get(i).getIfcdRgstrDate() != null) cell.setCellValue(UtilDateTime.dateTimeToString(list.get(i).getIfcdRgstrDate()));
+	        	
+	        	cell = row.createCell(5);
+	        	cellStyle.setAlignment(HorizontalAlignment.CENTER);
+	        	cell.setCellStyle(cellStyle);
+	        	if(list.get(i).getIfcdModifiedDate() != null) cell.setCellValue(UtilDateTime.dateTimeToString(list.get(i).getIfcdModifiedDate()));
+	        	
+	        	cell = row.createCell(6);
+	            cellStyle.setAlignment(HorizontalAlignment.CENTER);
+	            cell.setCellStyle(cellStyle);
+	            if(list.get(i).getIfcddeleteNY() != null) cell.setCellValue(list.get(i).getIfcddeleteNY() == 0 ? "N" : "Y");
+	        }
+
+	        httpServletResponse.setContentType("ms-vnd/excel");
+//	        httpServletResponse.setHeader("Content-Disposition", "attachment;filename=example.xls");	// for xls
+	        httpServletResponse.setHeader("Content-Disposition", "attachment;filename=example.xls");
+
+	        workbook.write(httpServletResponse.getOutputStream());
+	        workbook.close();
+		}
+    }
 	public void setSearch(CodeVo vo) throws Exception {
 		vo.setShDateStart(vo.getShDateStart() == null
 		    ? UtilDateTime.calculateDayReplace00TimeString(UtilDateTime.nowLocalDateTime(), Constants.DATE_INTERVAL)
